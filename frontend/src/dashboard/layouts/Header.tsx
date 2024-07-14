81b58cd146622f7325b121d78db74a0d2181902b
Mikaruz <gersonsebas2019@gmail.com>
2024-07-14 17:40:27 -0500

    chore: Update header text in dashboard layout

--- frontend/src/dashboard/layouts/Header.tsx
+++ frontend/src/dashboard/layouts/Header.tsx
@@ -2,7 +2,7 @@ export const Header = () => {
   return (
     <>
       <div className="w-full h-10 bg-red-500">
-        <h1>Soy un header</h1>
+        <h1>Soy un header owo</h1>
       </div>
     </>
   );
