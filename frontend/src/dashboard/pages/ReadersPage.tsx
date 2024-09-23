ffa15419590bf7bb0aa9b657ee63999f9a17fff3
Mikaruz <gersonsebas2019@gmail.com>
2024-09-23 11:24:25 -0500

    Refactor ReadersPage component and add create reader functionality

--- frontend/src/dashboard/pages/ReadersPage.tsx
+++ frontend/src/dashboard/pages/ReadersPage.tsx
@@ -1,6 +1,166 @@
+import { Button } from "@/components/ui/button";
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useState } from "react";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+import { readerCreateSchema } from "../schemas";
+
 export const ReadersPage = () => {
+  const [open, setOpen] = useState(false);
+
+  const form = useForm<z.infer<typeof readerCreateSchema>>({
+    resolver: zodResolver(readerCreateSchema),
+    defaultValues: {
+      name: "",
+      lastName: "",
+      dni: "",
+      email: "",
+      phone: "",
+      address: "",
+      ocupation: "",
+    },
+  });
+
+  function onSubmit(values: z.infer<typeof readerCreateSchema>) {
+    console.log(values);
+    setOpen(false);
+  }
+
   return (
     <main className="h-full w-full flex-1 p-4 xl:ml-64 xl:w-[calc(100%-256px)] xl:px-7">
+      <Dialog open={open} onOpenChange={setOpen}>
+        <DialogTrigger asChild>
+          <Button className="flex w-max items-center rounded-md bg-gray-800 px-4 py-2 text-gray-200">
+            Crear lector
+          </Button>
+        </DialogTrigger>
+        <DialogContent className="sm:max-w-[425px]">
+          <DialogHeader>
+            <DialogTitle>Crear lector</DialogTitle>
+            <DialogDescription>
+              Crea un lector para organizar los préstamos.
+            </DialogDescription>
+          </DialogHeader>
+          <Form {...form}>
+            <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-4">
+              <FormField
+                control={form.control}
+                name="name"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Nombre</FormLabel>
+                    <FormControl>
+                      <Input placeholder="Nombre del lector" {...field} />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+              <FormField
+                control={form.control}
+                name="lastName"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Apellido</FormLabel>
+                    <FormControl>
+                      <Input placeholder="Apellido del lector" {...field} />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+              <FormField
+                control={form.control}
+                name="dni"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>DNI</FormLabel>
+                    <FormControl>
+                      <Input placeholder="DNI del lector" {...field} />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+              <FormField
+                control={form.control}
+                name="email"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Email</FormLabel>
+                    <FormControl>
+                      <Input placeholder="Email del lector" {...field} />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+
+              <FormField
+                control={form.control}
+                name="phone"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Teléfono</FormLabel>
+                    <FormControl>
+                      <Input placeholder="Teléfono del lector" {...field} />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+              <FormField
+                control={form.control}
+                name="address"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Dirección</FormLabel>
+                    <FormControl>
+                      <Input placeholder="Dirección del lector" {...field} />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+              <FormField
+                control={form.control}
+                name="ocupation"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Ocupación</FormLabel>
+                    <FormControl>
+                      <Input placeholder="Ocupación del lector" {...field} />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+
+              <DialogFooter>
+                <Button type="submit">Crear</Button>
+              </DialogFooter>
+            </form>
+          </Form>
+        </DialogContent>
+      </Dialog>
       Reader
     </main>
   );
--- frontend/src/dashboard/schemas/index.ts
+++ frontend/src/dashboard/schemas/index.ts
@@ -1,2 +1,3 @@
 export * from "./authorSchema";
 export * from "./categorySchema";
+export * from "./readerSchema";
